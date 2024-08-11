69456c468be6106c236978f1815bb5511e180b6f
Facuu16 <hincfacundo@gmail.com>
2024-08-11 19:38:28 -0300

    style: make slight changes to MinecraftExceptionHandler in CommandManager

--- command/src/main/java/io/github/facuu16/flib/command/CommandManager.java
+++ command/src/main/java/io/github/facuu16/flib/command/CommandManager.java
@@ -3,7 +3,6 @@ package io.github.facuu16.flib.command;
 import io.github.facuu16.flib.annotation.Component;
 import io.github.facuu16.flib.annotation.Plugin;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.incendo.cloud.SenderMapper;
@@ -13,8 +12,6 @@ import org.incendo.cloud.minecraft.extras.MinecraftExceptionHandler;
 import org.incendo.cloud.minecraft.extras.MinecraftHelp;
 import org.incendo.cloud.paper.LegacyPaperCommandManager;
 import team.unnamed.inject.Inject;
-
-import static net.kyori.adventure.text.Component.text;
 
 @Component
 public class CommandManager extends LegacyPaperCommandManager<CommandSender> {
@@ -33,18 +30,7 @@ public class CommandManager extends LegacyPaperCommandManager<CommandSender> {
         }
 
         MinecraftExceptionHandler.create(audiences::sender)
-                .defaultInvalidSyntaxHandler()
-                .defaultInvalidSenderHandler()
-                .defaultNoPermissionHandler()
-                .defaultArgumentParsingHandler()
-                .defaultCommandExecutionHandler()
-                .decorator(
-                        component -> text()
-                                .append(text("[", NamedTextColor.DARK_GRAY))
-                                .append(text("Example", NamedTextColor.GOLD))
-                                .append(text("] ", NamedTextColor.DARK_GRAY))
-                                .append(component).build()
-                )
+                .defaultHandlers()
                 .registerTo(this);
 
         captionRegistry().registerProvider(MinecraftHelp.defaultCaptionsProvider());
